6f85fe36653ef41c01a92fc170f205e86d88c637
1Clavero <arturoclavero01@gmail.com>
2024-07-22 21:02:51 +0700

    fix(delimeter updated)

--- src/parsing/add_light.c
+++ src/parsing/add_light.c
@@ -6,7 +6,7 @@
 /*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 21:41:01 by uolle             #+#    #+#             */
-/*   Updated: 2024/07/22 17:58:45 by arturo           ###   ########.fr       */
+/*   Updated: 2024/07/22 21:02:00 by arturo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ void	add_amb_light_parsing(t_pars **pars, char *line)
 	elem.type = AMBIENT;
 	ft_split_tokens(line + 2, tokens, 4);
 	elem.brightness = ft_atof(tokens[0]);
-	
 	printf("tokens[0]: %s\n", tokens[0]);
 	printf("tokens[1]: %s\n", tokens[1]);
 	printf("tokens[2]: %s\n", tokens[2]);
 	create_tupple(&elem.color_range255, ft_atoi(tokens[1]), ft_atoi(tokens[2]),
 		ft_atoi(tokens[3]));
-	if (check_limit_color(elem.color_range255) == 0)
+	if (check_limit_color(elem.color_range255) == 0 
+		|| check_limit_value(1, 0, -42, elem.brightness) == 0)
 		return ;
 	add_element_to_pars_list(elem, pars);
 }
@@ -44,9 +44,8 @@ void	add_dif_light_parsing(t_pars **pars, char *line)
 	elem.brightness = ft_atof(tokens[3]);
 	create_tupple(&elem.color_range255, ft_atoi(tokens[4]), ft_atoi(tokens[5]),
 		ft_atoi(tokens[6]));
-	if (check_limit_value(1.0f, 0.0f, -42, elem.brightness))
-		return ;
-	if (check_limit_color(elem.color_range255) == 0)
+	if (check_limit_value(1.0f, 0.0f, -42, elem.brightness) == 0
+		|| check_limit_color(elem.color_range255) == 0 )
 		return ;
 	add_element_to_pars_list(elem, pars);
 }
@@ -62,8 +61,8 @@ void	add_spec_light_parsing(t_pars **pars, char *line)
 	ft_split_tokens(line + 2, tokens, 2);
 	elem.brightness = ft_atof(tokens[0]);
 	elem.shine = ft_atof(tokens[1]);
-	if (check_limit_value(1.0f, 0.0f, -42, elem.brightness) 
-		|| check_limit_value(250.0f, 0.0f, -42, elem.shine))
+	if (check_limit_value(1.0f, 0.0f, -42, elem.brightness == 0) 
+		|| check_limit_value(250.0f, 0.0f, SPECULAR , elem.shine) == 0)
 		return ;
 	add_element_to_pars_list(elem, pars);
 }
